19ed324cee1ad4e51d17faadc8e4e954ecd6f921
13-Rob <rcgr.13@gmail.com>
2023-04-05 12:32:24 -0700

    Change car color

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -6,6 +6,14 @@ public class Delivery : MonoBehaviour
 {
     bool hasPackage;
     [SerializeField] float destroyDelay = 0.5f;
+    [SerializeField] Color32 hasPackageColor = new Color32(1, 1, 1, 255);
+    [SerializeField] Color32 noPackageColor = new Color32(0, 0, 0, 255);
+
+    SpriteRenderer spriteRenderer;
+
+    void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("Collision");
@@ -16,12 +24,20 @@ public class Delivery : MonoBehaviour
         if (other.tag == "Package" && !hasPackage) {
             hasPackage = true;
             Debug.Log("hasPackage: " + hasPackage);
+
+            // Change subject color
+            spriteRenderer.color = hasPackageColor;
+
+            // Destroy passed object
             Destroy(other.gameObject, destroyDelay);
         }
 
         if (other.tag == "Destiny" && hasPackage) {
             hasPackage = false;
             Debug.Log("hasPackage: " + hasPackage);
+
+            // Change subject color
+            spriteRenderer.color = noPackageColor;
         }
     }
 }
