8d133ecf9ea2cdad6912da29461dcc0f736d07f6
13-Rob <rcgr.13@gmail.com>
2023-04-05 12:59:17 -0700

    Change of speed

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -5,17 +5,12 @@ using UnityEngine;
 public class Driver : MonoBehaviour
 {
     [SerializeField] float steerSpeed = 1f;
-    [SerializeField] float moveSpeed = 0.1f;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
+    [SerializeField] float moveSpeed = 15f;
+    [SerializeField] float slowSpeed = 10f;
+    [SerializeField] float boostSpeed = 30f;
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
         float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
 
@@ -24,5 +19,15 @@ public class Driver : MonoBehaviour
 
         //Moves
         transform.Translate(0, moveAmount, 0);
+    }
+
+    void OnCollisionEnter2D(Collision2D other) {
+        moveSpeed = slowSpeed;
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Boost") {
+            moveSpeed = boostSpeed;
+        }
     }
 }
